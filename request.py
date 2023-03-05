fde4f490f3e5a6bdb2faa98e9a543dc278db754e
Dhirodatto Biswas <83452888+vardhiro@users.noreply.github.com>
2023-03-05 15:13:48 +0530

    Update request.py

--- request.py
+++ request.py
@@ -5,7 +5,7 @@ import openai
 
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = "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"
+openai.api_key = "api_key_here"
 
 response = openai.Completion.create(model="text-davinci-003", prompt=sys.argv[1], temperature=0, max_tokens=700)
 
